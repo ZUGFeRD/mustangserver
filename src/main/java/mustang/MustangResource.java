de9e3297f50a18b978777d7bf430cf84c25967ee
Jochen Stärk <jstaerk@usegroup.de Jochen Staerk>
2019-08-17 17:57:17 +0200

    Remove the need for a temp file

--- src/main/java/mustang/MustangResource.java
+++ src/main/java/mustang/MustangResource.java
@@ -1,7 +1,6 @@
 package mustang;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,16 +18,14 @@ import org.slf4j.LoggerFactory;
 
 import io.swagger.annotations.Api;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.util.Optional;
+import java.io.PipedInputStream;
 
 @Path("/mustang")
 @Produces(MediaType.APPLICATION_JSON)
@@ -62,14 +59,9 @@ public class MustangResource {
 			@FormDataParam("file") final FormDataContentDisposition contentDispositionHeader, @FormDataParam("xml") String XML) {
 
 		ZUGFeRDExporter ze;
-   	    File tempFile = null;
-		try {
-			tempFile = File.createTempFile("invoice", ".tmp");
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
+   	    
+		ByteArrayOutputStream output=new ByteArrayOutputStream();
+		
 		try {
 			logger.debug("Converting to PDF/A-3u");
 
@@ -83,23 +75,18 @@ public class MustangResource {
 			ze.setZUGFeRDXMLData(XML.getBytes("UTF-8"));
 			logger.debug("Writing ZUGFeRD-PDF");
 	            
-			ze.export(tempFile.getAbsolutePath());
+			ze.export(output);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		InputStream is;
-		try {
-			is = new FileInputStream(tempFile.getAbsolutePath());
-	        return Response.ok(is)
+		}    
+		byte[] bytes = output.toByteArray();
+		InputStream inputStream = new ByteArrayInputStream(bytes);
+		
+		return Response.ok(inputStream)
 	                .header(HttpHeaders.CONTENT_DISPOSITION, 
 	                        "attachment; filename=\"invoice.pdf\"")
 	                .build();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-
+		
 	}
 
 }
